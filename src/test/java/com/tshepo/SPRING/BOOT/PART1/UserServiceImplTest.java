238f0e2c8944f7779f88eaa05ad5db1a74310b32
crespo21 <59964121+crespo21@users.noreply.github.com>
2020-05-13 15:46:43 +0200

    Update UserServiceImplTest.java

--- src/test/java/com/tshepo/SPRING/BOOT/PART1/UserServiceImplTest.java
+++ src/test/java/com/tshepo/SPRING/BOOT/PART1/UserServiceImplTest.java
@@ -1,15 +1,30 @@
 package com.tshepo.SPRING.BOOT.PART1;
-import com.tshepo.SPRING.BOOT.PART1.model.User;
+import com.tshepo.SPRING.BOOT.PART1.Dao.FakeRepo;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class UserServiceImplTest {
 
     @Test
     public void testInsertUser() {
-        assertArrayEquals(new User(1,"tshepo","seleke"));
-
+        assertThat(Boolean.parseBoolean(
+                new FakeRepo().insertUser(1,
+                        "theo","park") + " is entered"));
     }
 
-    private void assertArrayEquals(User user) {
+    @Test
+    public void testFindUserById(){
+        assertThat(Boolean.parseBoolean(new FakeRepo().findUserById(1)));
+    }
+
+    @Test
+    public void deleteUserTest(){
+        assertThat(Boolean.parseBoolean(new FakeRepo().deleteUser(1)+" is removed"));
+    }
+
+    @Test
+    public void UserTest(){
+        assertThat(Boolean.parseBoolean("Hello " + new FakeRepo().User(1)));
     }
 }
